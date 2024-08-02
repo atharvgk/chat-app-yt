fcbfc0cfe3c509b97cc115efdd154cb5a5b5599e
atharvgk <atharv.katyarmal@gmail.com>
2024-08-02 18:39:06 +0530

    backend completed (get users for sidebar)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();
 const PORT=process.env.PORT || 5000;
@@ -15,6 +19,7 @@ app.use(cookieParser());
 
 app.use('/api/auth',authRoutes );
 app.use('/api/messages',messageRoutes );
+app.use('/api/users',userRoutes );
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
